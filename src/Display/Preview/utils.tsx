92a5f4f0ed17d853477b502f74d9f2c31ae38105
Rangocold <704369725@qq.com>
2022-01-21 18:42:19 +0800

    stash

--- src/Display/Preview/utils.tsx
+++ src/Display/Preview/utils.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Tooltip } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
 
 export function wrapLabelWithHint(label?: string, hint?: string) {
   if (hint) {
-    return <p>{label}<InfoCircleOutlined /></p>;
+    return (
+      <p>
+        {label}
+        <Tooltip title={hint}>
+          <InfoCircleOutlined />
+        </Tooltip>
+      </p>
+    );
   } else {
     return label;
   }
